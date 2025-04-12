bba9bb11d64bd293939801e5f8ade8f2111bcb80
Alice Silva <alicelspires@gmail.com>
2025-04-12 01:56:00 -0300

    Organizing files

--- java/Main1003.java
+++ java/Main1003.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class Main
-{
+public class Main1003{
 	public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 	    int A = scanner.nextInt();
